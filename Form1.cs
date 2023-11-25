cde97e5b2bdb477db32c6e88b9074a415ad9d38e
NQuangMin <minh19102003@gmail.com>
2023-11-25 17:13:53 +0700

    original

--- Form1.cs
+++ Form1.cs
@@ -183,9 +183,9 @@ namespace Test_PTUDCSDL
             }
             catch
             {
-                MessageBox.Show("Lỗi", "Thông báo");//this is a new comment
+                MessageBox.Show("Lỗi", "Thông báo");
             }
-            //this is a comment
+            
         }
     }
 }
